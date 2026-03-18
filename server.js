fe81844ecda0b8fa0ee0beee084ea2ca1a25b2ea
Flutura Hyseni <flutura.hyseni@umib.net>
2026-03-18 22:46:43 +0100

    Added service layer and updated server configuration

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+}); 
+//Testimi per databaze
+// const pool = require('./src/config/db');
+
+// pool.connect()
+//   .then(() => console.log("Database connected"))
+//   .catch(err => console.error("Database error:", err));
