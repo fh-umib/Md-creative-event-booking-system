bcb4ab28c12f3268df0f3e19b23ee79c6deb7e9b
Flutura Hyseni <flutura.hyseni@umib.net>
2026-05-05 09:53:56 +0200

    feat: update Vite configuration to enhance build chunking and organization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-router-dom')) {
+              return 'router';
+            }
+
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react';
+            }
+
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
